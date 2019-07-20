f76230b0d16b7ed23a2daa4e1ccc734e56ab81c0
Victor Bouvier-Deleau <victor.bouvier-deleau@kolabnow.com>
2019-07-20 02:10:10 +0200

    Fix NFS mounting in Vagrant

--- vagrant/simul_prod_env.sh
+++ vagrant/simul_prod_env.sh
@@ -3,7 +3,8 @@
 readonly EXPORT_ROOT="/srv/nfs"
 readonly EXPORT_DIRS="${EXPORT_ROOT}/cloud/data ${EXPORT_ROOT}/downloads/bittorrent/{leeching,seeding,watching} \
 ${EXPORT_ROOT}/downloads/usenet/{completed,processing,watching} \
-${EXPORT_ROOT}/medias/{audio_drama,audiobooks,books,comics,movies,music,podcasts,tv_shows} ${EXPORT_ROOT}/sync"
+${EXPORT_ROOT}/medias/{audio_drama,audiobooks,books,comics,movies,music,podcasts,tv_shows} \
+${EXPORT_ROOT}/sync/vagrant/{documents,pictures,videos}"
 
 yum upgrade -y
 yum install -y firewalld nfs-utils rpcbind rsync
@@ -16,23 +17,30 @@ systemctl enable --now rpcbind nfs-server
 # I know, eval is evil. But this is the only way to have variable expansion before the brace expansion so that
 # "mkdir {folder1,folder2}" creates 2 folders instead of 1 folder named litteraly "{folder1,folder2}".
 eval "mkdir -p ${EXPORT_DIRS}"
+chown 80:80 ${EXPORT_ROOT}/cloud/data && chmod 750 ${EXPORT_ROOT}/cloud/data
+chown root:1004 ${EXPORT_ROOT}/downloads/bittorrent/* && chmod 770 ${EXPORT_ROOT}/downloads/bittorrent/*
+chown root:1004 ${EXPORT_ROOT}/downloads/usenet/* && chmod 770 ${EXPORT_ROOT}/downloads/usenet/*
+chown root:1005 ${EXPORT_ROOT}/medias/* && chmod 775 ${EXPORT_ROOT}/medias/*
+chown 1000:1001 ${EXPORT_ROOT}/sync/vagrant/* && chmod 755 ${EXPORT_ROOT}/sync/user/*
 cat << EOF > /etc/exports
-${EXPORT_ROOT}/cloud/data localhost(rw)
-${EXPORT_ROOT}/downloads/bittorrent/leeching localhost(rw)
-${EXPORT_ROOT}/downloads/bittorrent/seeding localhost(rw)
-${EXPORT_ROOT}/downloads/bittorrent/watching localhost(rw)
-${EXPORT_ROOT}/downloads/usenet/completed localhost(rw)
-${EXPORT_ROOT}/downloads/usenet/processing localhost(rw)
-${EXPORT_ROOT}/downloads/usenet/watching localhost(rw)
-${EXPORT_ROOT}/medias/audio_drama localhost(rw)
-${EXPORT_ROOT}/medias/audiobooks localhost(rw)
-${EXPORT_ROOT}/medias/books localhost(rw)
-${EXPORT_ROOT}/medias/comics localhost(rw)
-${EXPORT_ROOT}/medias/movies localhost(rw)
-${EXPORT_ROOT}/medias/music localhost(rw)
-${EXPORT_ROOT}/medias/podcasts localhost(rw)
-${EXPORT_ROOT}/medias/tv_shows localhost(rw)
-${EXPORT_ROOT}/sync localhost(rw)
+${EXPORT_ROOT}/cloud/data localhost(rw,all_squash,anonuid=80,anongid=80)
+${EXPORT_ROOT}/downloads/bittorrent/leeching localhost(rw,all_squash,anonuid=65534,anongid=1004)
+${EXPORT_ROOT}/downloads/bittorrent/seeding localhost(rw,all_squash,anonuid=65534,anongid=1004)
+${EXPORT_ROOT}/downloads/bittorrent/watching localhost(rw,all_squash,anonuid=65534,anongid=1004)
+${EXPORT_ROOT}/downloads/usenet/completed localhost(rw,all_squash,anonuid=65534,anongid=1004)
+${EXPORT_ROOT}/downloads/usenet/processing localhost(rw,all_squash,anonuid=65534,anongid=1004)
+${EXPORT_ROOT}/downloads/usenet/watching localhost(rw,all_squash,anonuid=65534,anongid=1004)
+${EXPORT_ROOT}/medias/audio_drama localhost(rw,all_squash,anonuid=65534,anongid=1005)
+${EXPORT_ROOT}/medias/audiobooks localhost(rw,all_squash,anonuid=65534,anongid=1005)
+${EXPORT_ROOT}/medias/books localhost(rw,all_squash,anonuid=65534,anongid=1005)
+${EXPORT_ROOT}/medias/comics localhost(rw,all_squash,anonuid=65534,anongid=1005)
+${EXPORT_ROOT}/medias/movies localhost(rw,all_squash,anonuid=65534,anongid=1005)
+${EXPORT_ROOT}/medias/music localhost(rw,all_squash,anonuid=65534,anongid=1005)
+${EXPORT_ROOT}/medias/podcasts localhost(rw,all_squash,anonuid=65534,anongid=1005)
+${EXPORT_ROOT}/medias/tv_shows localhost(rw,all_squash,anonuid=65534,anongid=1005)
+${EXPORT_ROOT}/sync/vagrant/documents localhost(rw,all_squash,anonuid=1000,anongid=1001)
+${EXPORT_ROOT}/sync/vagrant/pictures localhost(rw,all_squash,anonuid=1000,anongid=1001)
+${EXPORT_ROOT}/sync/vagrant/videos localhost(rw,all_squash,anonuid=1000,anongid=1001)
 EOF
 restorecon /etc/exports
 systemctl restart nfs-server
