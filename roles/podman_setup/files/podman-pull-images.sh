900cb8e2588fa23394681e566c48f9f155e7b611
Victor Bouvier-Deleau <victor@bouvier-deleau.fr>
2025-02-19 21:58:12 +0100

    Configure OS and containers auto updates

---
+++ roles/podman_setup/files/podman-pull-images.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [[ ${EUID} -ne 0 ]]; then
+  echo "Must be executed as root." 1>&2
+  exit 1
+fi
+
+echo "Pulling new images"
+
+for image in $(grep -h -r "Image=" /etc/containers/systemd/ | awk -F= '{print $2}' | uniq); do
+  podman image pull "${image}"
+done
