4303e5ea68a34726bc208c717f73e3fc1c9b9b1e
Victor Bouvier-Deleau <victor@bouvier-deleau.fr>
2025-02-24 17:07:50 +0100

    Disable ShellCheck warning because we need word splitting

--- roles/podman_setup/files/podman-prune-old-tags.sh
+++ roles/podman_setup/files/podman-prune-old-tags.sh
@@ -10,5 +10,6 @@ fi
 echo "Removing images which have a more recent tag"
 
 for image in $(podman image ls --format "{{.Repository}} {{.Tag}}" | grep -v "<none>" | sort | awk '{print $1}' | uniq -d); do
+  # shellcheck disable=SC2046
   podman image rm -f $(podman image ls --format "{{.Repository}}:{{.Tag}}" "${image}" | sort | head -n -1 | tr '\n' ' ');
 done
