6f96d49dd614a7b0edd2e4f77e9e4698538e2fe5
Victor Bouvier-Deleau <victor@bouvier-deleau.fr>
2025-08-21 01:11:42 +0200

    Change podman-pull-images error handling
    With set -e the script will stop processing pulls if an image is not available anymore on a registry but I want it to continue processing pulls anyway and just return a fail at the end

--- roles/podman_setup/files/podman-pull-images.sh
+++ roles/podman_setup/files/podman-pull-images.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
-set -e
-
 if [[ ${EUID} -ne 0 ]]; then
   echo "Must be executed as root." 1>&2
   exit 1
 fi
 
+podman_pull_failed=0
+
 echo "Pulling new images"
 
 for image in $(grep -s -h -r "Image=" /etc/containers/systemd/ /etc/systemd/system/ | awk -F= '{print $2}' | uniq); do
-  podman image pull "${image}"
+  podman image pull "${image}" || podman_pull_failed=1
 done
+
+[[ ${podman_pull_failed} -ne 0 ]] && exit 1 || exit 0
